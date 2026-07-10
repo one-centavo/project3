983e97f6fefbbb902f4558718496bf2d8dde8042
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-07-10 13:56:24 -0500

    feat(client): add Cliente model

---
+++ app/Models/Cliente.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'cliente';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'uuid';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'uuid',
+        'dni',
+        'first_name',
+        'second_name',
+        'first_last_name',
+        'second_last_name',
+        'email',
+        'phone_number',
+        'address',
+        'is_sync',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'is_sync' => 'boolean',
+    ];
+}
