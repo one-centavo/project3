ccc61834da6721b36121507c18b7c30a392c13b4
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-07-17 18:24:31 -0500

    feat(pwa): register service worker in entry script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-import './offline-db';
+import "./offline-db";
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/sw.js")
+            .then((reg) =>
+                console.log(
+                    "Single Service Worker registered at root!",
+                    reg,
+                ),
+            )
+            .catch((err) => console.error("Error registering Service Worker:", err));
+    });
+}
