463e9e3f79d9b24987832e2185ca7aba1991770b
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-07-17 18:24:39 -0500

    feat(pwa): add route to serve manifest.webmanifest

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::livewire('/', 'clients/create-client')->name('clients.create');
+
+Route::get('/manifest.webmanifest', function () {
+    return Response::file(public_path('build/manifest.webmanifest'), [
+        'Content-Type' => 'application/manifest+json',
+    ]);
+});
