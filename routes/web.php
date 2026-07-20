8019ccce9813d3501210e35df79bbcb5547677df
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-07-20 05:52:06 -0500

    feat(clients): add manager wrapper and update routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::livewire('/', 'clients/create-client')->name('clients.create');
+Route::livewire('/', 'clients/manager')->name('clients.manager');
 
 Route::get('/manifest.webmanifest', function () {
     return Response::file(public_path('build/manifest.webmanifest'), [
